ramchaik/AstroNvim

Move to new 0.7 lua APIs and global statusbar
Signed-off-by: Micah Halter <micah@balena.io>

lua/configs/lsp/handlers.lua
@@ -72,7 +72,7 @@ M.on_attach = function(client, bufnr)
     on_attach_override(client, bufnr)
   end
 
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_add_user_command("Format", vim.lsp.buf.formatting, {})
   lsp_highlight_document(client)
 end
 

lua/core/autocmds.lua
@@ -3,34 +3,52 @@ local M = {}
 local utils = require "core.utils"
 local config = utils.user_settings()
 
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]]
+local cmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local add_command = vim.api.nvim_add_user_command
 
-vim.cmd [[
-  augroup cursor_off
-    autocmd!
-    autocmd WinLeave * set nocursorline
-    autocmd WinEnter * set cursorline
-  augroup end
-]]
+augroup("packer_user_config", {})
+cmd("BufWritePost", {
+  desc = "Auto Compile plugins.lua file",
+  group = "packer_user_config",
+  command = "source <afile> | PackerCompile",
+  pattern = "plugins.lua",
+})
+
+augroup("cursor_off", {})
+cmd("WinLeave", {
+  desc = "No cursorline",
+  group = "cursor_off",
+  command = "set nocursorline",
+})
+cmd("WinEnter", {
+  desc = "No cursorline",
+  group = "cursor_off",
+  command = "set cursorline",
+})
 
 if config.enabled.dashboard and config.enabled.bufferline then
-  vim.cmd [[
-    augroup dashboard_settings
-      autocmd!
-      autocmd FileType dashboard set showtabline=0
-      autocmd BufWinLeave <buffer> set showtabline=2
-      autocmd BufEnter * if &ft is "dashboard" | set nocursorline | endif
-    augroup end
-  ]]
+  augroup("dashboard_settings", {})
+  cmd("FileType", {
+    desc = "Disable tabline for dashboard",
+    group = "dashboard_settings",
+    pattern = "dashboard",
+    command = "set showtabline=0",
+  })
+  cmd("BufWinLeave", {
+    desc = "Reenable tabline when leaving dashboard",
+    group = "dashboard_settings",
+    pattern = "<buffer>",
+    command = "set showtabline=2",
+  })
+  cmd("BufEnter", {
+    desc = "No cursorline on dashboard",
+    group = "dashboard_settings",
+    pattern = "*",
+    command = "if &ft is 'dashboard' | set nocursorline | endif",
+  })
 end
 
-vim.cmd [[
-  command! AstroUpdate lua require('core.utils').update()
-]]
+add_command("AstroUpdate", require("core.utils").update, {})
 
 return M

Bacbia3696/AstroVim

lua/configs/lsp/handlers.lua
@@ -72,7 +72,7 @@ M.on_attach = function(client, bufnr)
     on_attach_override(client, bufnr)
   end
 
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
   lsp_highlight_document(client)
 end
 

lua/core/autocmds.lua
@@ -2,34 +2,52 @@ local M = {}
 
 local utils = require "core.utils"
 
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]]
+local cmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local create_command = vim.api.nvim_create_user_command
 
-vim.cmd [[
-  augroup cursor_off
-    autocmd!
-    autocmd WinLeave * set nocursorline
-    autocmd WinEnter * set cursorline
-  augroup end
-]]
+augroup("packer_user_config", {})
+cmd("BufWritePost", {
+  desc = "Auto Compile plugins.lua file",
+  group = "packer_user_config",
+  command = "source <afile> | PackerCompile",
+  pattern = "plugins.lua",
+})
+
+augroup("cursor_off", {})
+cmd("WinLeave", {
+  desc = "No cursorline",
+  group = "cursor_off",
+  command = "set nocursorline",
+})
+cmd("WinEnter", {
+  desc = "No cursorline",
+  group = "cursor_off",
+  command = "set cursorline",
+})
 
 if utils.is_available "dashboard-nvim" and utils.is_available "bufferline.nvim" then
-  vim.cmd [[
-    augroup dashboard_settings
-      autocmd!
-      autocmd FileType dashboard set showtabline=0
-      autocmd BufWinLeave <buffer> set showtabline=2
-      autocmd BufEnter * if &ft is "dashboard" | set nocursorline | endif
-    augroup end
-  ]]
+  augroup("dashboard_settings", {})
+  cmd("FileType", {
+    desc = "Disable tabline for dashboard",
+    group = "dashboard_settings",
+    pattern = "dashboard",
+    command = "set showtabline=0",
+  })
+  cmd("BufWinLeave", {
+    desc = "Reenable tabline when leaving dashboard",
+    group = "dashboard_settings",
+    pattern = "<buffer>",
+    command = "set showtabline=2",
+  })
+  cmd("BufEnter", {
+    desc = "No cursorline on dashboard",
+    group = "dashboard_settings",
+    pattern = "*",
+    command = "if &ft is 'dashboard' | set nocursorline | endif",
+  })
 end
 
-vim.cmd [[
-  command! AstroUpdate lua require('core.utils').update()
-]]
+create_command("AstroUpdate", require("core.utils").update, {})
 
 return M